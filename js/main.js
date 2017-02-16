a6af47321c446d821d4a87c6a6807a8eec11f3e7
Iffahdahiyah <iffahdahiyah@gmail.com>
2017-02-16 22:00:17 +0800

    Added interactivity and jquery
    This is fun

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ var studioLink = $('.studio');
 studioLink.on('click', function(){
   console.log("cats")
 })
+
+var menuCats = $("#menu-cats");
+var menuTravels = $("#menu-travels");
+var menuSports = $("#menu-sports");
+var menuStudio = $("#menu-studio");
+
+var itemsCats = $(".cats");
+var itemsTravels = $(".travels");
+var itemsSports = $(".sports");
+// var itemsStudio = $("");
+
+menuCats.mouseover(function(){
+  itemsCats.addClass("selected")
+  itemsTravels.removeClass("selected")
+  itemsSports.removeClass("selected")
+})
+
+menuTravels.mouseover(function(){
+  itemsTravels.addClass("selected")
+  itemsCats.removeClass("selected")
+  itemsSports.removeClass("selected")
+})
+
+menuSports.mouseover(function(){
+  itemsSports.addClass("selected")
+  itemsCats.removeClass("selected")
+  itemsTravels.removeClass("selected")
+})
